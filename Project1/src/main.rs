be6913a61f35d5497d41f6cebed591e6f56c5a65
HenryJohnson26 <108373639+HenryJohnson26@users.noreply.github.com>
2024-02-03 12:14:11 -0700

    Properly adding edges to state graph

--- Project1/src/main.rs
+++ Project1/src/main.rs
@@ -1,12 +1,16 @@
 use core::num;
+use std::borrow::Borrow;
 use std::{env, string};
 use std::fs;
+use std::collections::HashMap;
 
+use petgraph::data::Build;
 use petgraph::graph::Graph;
+use petgraph::Directed;
 
 fn main() {
     let filePath: &str = "in.txt";
-    let mut gameGraph = Graph::<String, &str>::new();
+    let mut gameGraph = Graph::<String, i32>::new();
 
     let contents = fs::read_to_string(filePath)
         .expect("Should have been able to read the file");
@@ -29,18 +33,28 @@ fn main() {
     //print!("{:?}",startingState);
 
     //generate vertices
+    let mut nodeMap: HashMap<String, petgraph::prelude::NodeIndex> = HashMap::new();
+    //let mut nodeMap: HashMap<String, graph.nodeMap> = HashMap::new();
+    let end = gameGraph.add_node("end".to_string());
     for i in 1..numVertices+1{
         for j in 1..numVertices+1{
-            //println!("{:?},{:?}",i,j);
-            gameGraph.add_node(format!("{:?},{:?}",i,j));
+            let node = gameGraph.add_node(format!("{:?},{:?}",i,j));
+            nodeMap.insert(format!("{:?},{:?}",i,j), node);
+            if i == numVertices || j == numVertices{
+                gameGraph.add_edge(node, end, 0);
+            }
         }
     }
 
+
     //create goal vertice
-    gameGraph.add_node("end".to_string());
+    //gameGraph.add_node("end");
 
     //calculate edges
     let mut index = 0;
+    //let mut startNode = "".to_string();
+    let mut endNode = "".to_string();
+    let mut startNode = format!("balls");
     for line in inputLines{
         if index > 2 && line != ""{
             let input: Vec<&str> = line.get(0..line.len()-1).unwrap().split(' ').collect();
@@ -50,8 +64,12 @@ fn main() {
             //println!("{:?},{:?} color:{:?}",start, end, color);
             let mut verticeNum = 1;
             for vertexColor in &colorMap{
+                
                 if vertexColor.to_string() == color.to_string(){
-                    println!("Found available edges: {:?},{:?} to {:?},{:?} and {:?},{:?} to {:?},{:?}", start,verticeNum,end,verticeNum,verticeNum,start,verticeNum,end);
+                    //println!("Found available edges: {:?},{:?} to {:?},{:?} and {:?},{:?} to {:?},{:?}", start,verticeNum,end,verticeNum,verticeNum,start,verticeNum,end);
+                    //startNode = format!("{:?},{:?}",start,verticeNum);
+                    gameGraph.add_edge(*nodeMap.get(&format!("{:?},{:?}",start,verticeNum)).unwrap(), *nodeMap.get(&format!("{:?},{:?}",end,verticeNum)).unwrap(), 1);
+                    gameGraph.add_edge(*nodeMap.get(&format!("{:?},{:?}",verticeNum,start)).unwrap(), *nodeMap.get(&format!("{:?},{:?}",verticeNum,end)).unwrap(), 1);
                 }
                 verticeNum+=1;
             }
